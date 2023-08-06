0987245ff70bd09c907fa149afbd441c7af42566
netanel094 <netanel094@gmail.com>
2023-08-06 23:22:57 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="App">test</div>
+      <Row title="NETFLIX ORIGINALS" />
+      <Row title="Trending Now" />
+    </>
+  );
+}
+
+export default App;
---
+++ src/requests.js
@@ -0,0 +1,1 @@
+const API_KEY = "e5776e6e120f2e7c59281b95e707f0d5";
