f7fd054375c85930f483a3752c3ce05cf72e2060
netanel094 <netanel094@gmail.com>
2023-08-13 12:49:46 +0300

    Add "/3" to the path

--- src/requests.js
+++ src/requests.js
@@ -1,14 +1,14 @@
 const API_KEY = "e5776e6e120f2e7c59281b95e707f0d5";
 
 const request = {
-  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  fetchTrending: `/3/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/3/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/3/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/3/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/3/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/3/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/3/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/3/discover/movie?api_key=${API_KEY}&with_genres=99`,
 };
 
 export default request;
