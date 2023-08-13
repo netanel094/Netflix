b8c389b9f9ba0cd766cfbf414ed45338c57ba4c4
netanel094 <netanel094@gmail.com>
2023-08-13 17:17:48 +0300

    Add Banner component and indicate for large row

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import request from "./requests";
 import "./App.css";
 import Row from "./components/Row";
-
+import Banner from "./components/Banner";
 function App() {
   return (
-    <>
-      <div>test</div>
-      <Row title="NETFLIX ORIGINALS" fetchUrl={request.fetchNetflixOriginals} />
+    <div className="app">
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={request.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
       <Row title="Trending Now" fetchUrl={request.fetchTrending} />
       <Row title="Top Rated" fetchUrl={request.fetchTopRated} />
       <Row title="Action Movies" fetchUrl={request.fetchActionMovies} />
       <Row title="Comedy Movies" fetchUrl={request.fetchComedyMovies} />
       <Row title="Horror Movies" fetchUrl={request.fetchHorrorMovies} />
       <Row title="Romance Movies" fetchUrl={request.fetchRomanceMovies} />
-    </>
+    </div>
   );
 }
 
