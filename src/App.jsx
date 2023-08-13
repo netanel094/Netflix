14f5a3f7afb46c1196c6977f0d3f4b073ae83447
netanel094 <netanel094@gmail.com>
2023-08-13 12:48:35 +0300

    Add all rows

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,11 @@ function App() {
       <div>test</div>
       <Row title="NETFLIX ORIGINALS" fetchUrl={request.fetchNetflixOriginals} />
       <Row title="Trending Now" fetchUrl={request.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={request.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={request.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={request.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={request.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={request.fetchRomanceMovies} />
     </>
   );
 }
