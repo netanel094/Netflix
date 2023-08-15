a602390dcd571758624f60a5f33d8a36dd2adf20
netanel094 <netanel094@gmail.com>
2023-08-15 17:49:41 +0300

    Add vidoe trailer

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,18 +1,23 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
-
+import Youtube from "react-youtube";
 import "../Row.css";
 import useMovies from "../hooks/useMovies";
+import movieTrailer from "movie-trailer";
+
 const baseImgUrl = "https://image.tmdb.org/t/p/original/";
 
 export default function Row({ title, fetchUrl, isLargeRow }) {
   const [currentIndex, setCurrentIndex] = useState(0);
   const { movies, loading, error } = useMovies(fetchUrl);
 
+  const [trailerUrl, setTrailerUrl] = useState("");
+
   const move = (delta) => () => {
     const nextIndex = currentIndex + delta;
     const nextElement = document.getElementById(`${title}_${nextIndex}`);
     if (!nextElement) return;
+
     nextElement.scrollIntoView({
       behavior: "smooth",
       inline: "start",
@@ -27,6 +32,27 @@ export default function Row({ title, fetchUrl, isLargeRow }) {
   if (error) {
     return <h1>error</h1>;
   }
+
+  const options = {
+    height: "390",
+    width: "100%",
+    playerVars: { autoplay: 1 },
+  };
+
+  const handleClick = (movie) => {
+    console.log(movie?.name);
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
   return (
     <div className="row">
       <h2>{title}</h2>
@@ -37,6 +63,7 @@ export default function Row({ title, fetchUrl, isLargeRow }) {
         </button>
         {movies.map((movie, index) => (
           <img
+            onClick={() => handleClick(movie)}
             id={`${title}_${index}`}
             className={`row__poster ${isLargeRow && "row__posterLarge"}`}
             key={movie.id}
@@ -46,10 +73,12 @@ export default function Row({ title, fetchUrl, isLargeRow }) {
             alt={movie.name}
           />
         ))}
+
         <button className="next_btn" onClick={move(1)}>
           next
         </button>
       </div>
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={options} />}
     </div>
   );
 }
