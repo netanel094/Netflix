b72d3429d455e4aa4678c646946cb10e6b4e24bb
netanel094 <netanel094@gmail.com>
2023-08-15 18:11:20 +0300

    change protocol from http to https

--- src/requests.js
+++ src/requests.js
@@ -1,14 +1,14 @@
 const API_KEY = "e5776e6e120f2e7c59281b95e707f0d5";
 
 const request = {
-  fetchTrending: `http://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`,
-  fetchNetflixOriginals: `http://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213`,
-  fetchTopRated: `http://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-  fetchActionMovies: `http://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=28`,
-  fetchComedyMovies: `http://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=35`,
-  fetchHorrorMovies: `http://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=27`,
-  fetchRomanceMovies: `http://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-  fetchDocumentaries: `http://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  fetchTrending: `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=99`,
 };
 
 export default request;
