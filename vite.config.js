bda51ff360538e250eff57f8d363439698ef75d0
netanel094 <netanel094@gmail.com>
2023-08-15 18:06:55 +0300

    remove proxy from vite config

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/3": {
-        target: "http://api.themoviedb.org",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 });
