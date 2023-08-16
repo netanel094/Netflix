ac79338faf4ecce5e74bbbfa31002cdffa4a2c63
netanel094 <netanel094@gmail.com>
2023-08-16 13:06:05 +0300

    change the movie param from "name" to "title"

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -40,11 +40,10 @@ export default function Row({ title, fetchUrl, isLargeRow }) {
   };
 
   const handleClick = (movie) => {
-    console.log(movie?.name);
     if (trailerUrl) {
       setTrailerUrl("");
     } else {
-      movieTrailer(movie?.name || "")
+      movieTrailer(movie?.title || "")
         .then((url) => {
           const urlParams = new URLSearchParams(new URL(url).search);
           setTrailerUrl(urlParams.get("v"));
