2c59ab29644f9b148a15d908837deb3066872010
netanel094 <netanel094@gmail.com>
2023-08-16 13:13:40 +0300

    add movie?.title option for movieTrailer

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -43,7 +43,7 @@ export default function Row({ title, fetchUrl, isLargeRow }) {
     if (trailerUrl) {
       setTrailerUrl("");
     } else {
-      movieTrailer(movie?.title || "")
+      movieTrailer(movie?.title || movie?.title || "")
         .then((url) => {
           const urlParams = new URLSearchParams(new URL(url).search);
           setTrailerUrl(urlParams.get("v"));
